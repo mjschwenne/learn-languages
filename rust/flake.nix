8fbe365c7282ef9fbf0924a3ea4186339fdfc353
Matt Schwennesen <mjschwenne@gmail.com>
2025-06-26 15:39:27 -0500

    Add partially complete rust exercises

---
+++ rust/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Tools for completing 100 Exercises to Learn Rust Course";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              rust-bin.stable.latest.default
+              workshop-runner
+            ];
+
+            shellHook = ''
+            '';
+          };
+      }
+    );
+}
